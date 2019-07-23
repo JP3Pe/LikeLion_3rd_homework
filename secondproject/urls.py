5e47856d237ddcfaa72b5068f541bc13bda6bf9b
jpepe <rlaworhkd430430@gmail.com>
2019-07-23 18:20:27 +0900

    add social account function

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('portfolio/', include('portfolio.urls')),
     path('accounts/', include('accounts.urls')),
-    
+    path('social_accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
